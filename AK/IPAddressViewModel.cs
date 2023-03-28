eb92f0f233a7815f951644d02b18ae1ac188021c
Khoa-Nguyen1234 <NguyenKhoa020401@gmail.com>
2023-03-28 16:22:53 +0700

    AK

---
+++ AK/IPAddressViewModel.cs
@@ -0,0 +1,18 @@
+﻿using IPAddressManagement.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace IPAddressManagement.Areas.Admin.Models
+{
+    public class IPAddressViewModel
+    {
+
+        public int ID_IPAddress { get; set; }
+
+        [Required]
+        public string IPAddressName { get; set; }
+
+        [Required]
+        public bool Available { get; set; }
+
+    }
+}
---
+++ AK/UserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using IPAddressManagement.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
+
+namespace IPAddressManagement.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public int ID_User { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+
+        public string NewPassWord { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public int ID_Group { get; set; }
+
+    }
+}
