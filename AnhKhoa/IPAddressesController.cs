6a8a20a9fd976fd7198d62f6d9b932440942e7a0
Khoa-Nguyen1234 <NguyenKhoa020401@gmail.com>
2023-03-28 16:04:50 +0700

    new

---
+++ AnhKhoa/IPAddressesController.cs
@@ -0,0 +1,188 @@
+﻿using IPAddressManagement.Areas.Admin.Models;
+using IPAddressManagement.Areas.Admin.Repositories;
+using IPAddressManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace IPAddressManagement.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class IPAddressesController : BaseController
+    {
+        private IIpsRepository iPaddresssRepository;
+
+        public IPAddressesController(IIpsRepository iPaddresssRepository)
+        {
+            this.iPaddresssRepository = iPaddresssRepository;
+        }
+
+        // GET: GroupsController
+        public ActionResult Index()
+        {
+            var ipaddress = iPaddresssRepository.GetIps();
+            return View(ipaddress);
+        }        
+        
+        public ActionResult Available()
+        {
+            var ipaddress = iPaddresssRepository.GetIps().Where(i => i.Status == true);
+            return View(ipaddress);
+        }
+
+        
+        // GET: GroupsController/Create
+        public ActionResult Create()
+        {
+            return View(new IPAddressViewModel());
+        }
+
+        // POST: GroupsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IPAddressViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                if (!IsIPValid(model.IPAddressName))
+                {
+                    ModelState.AddModelError("IPAddressName", "Địa chỉ IP không hợp lệ");
+                    return View("Create", model);
+                }
+
+                if (iPaddresssRepository.ExistsIpByName(model.IPAddressName.Trim()))
+                {
+                    ModelState.AddModelError("IPAddressName", "Địa chỉ đã tồn tại trong hệ thống.");
+                    return View("Create", model);
+                }
+
+                IPAddresss ip = new IPAddresss();
+                ip.IPAddressName = model.IPAddressName.Trim();
+                ip.Status = model.Available;
+                ip.CreatedDate = DateTime.Now;
+                ip.CreatedBy = GetCurrentUser();
+
+
+                iPaddresssRepository.InsertIp(ip);
+                iPaddresssRepository.Save();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                // Handle the exception here
+                return View(model);
+            }
+        }
+
+        private bool IsIPValid(string ipAddress)
+        {
+            string[] octets = ipAddress.Split('.');
+            if (octets.Length != 4)
+                return false;
+
+            foreach(var octect in octets)
+            {
+                if(!IsOctectValid(octect)) 
+                    return false;
+            }
+            return true;
+        }
+
+        private bool IsOctectValid(string octect)
+        {
+            int num;
+            bool isNumeric = int.TryParse(octect, out num);
+            if (!isNumeric)
+                return false;
+            if(num < 0 || num > 255)
+                return false;
+            return true;
+        }
+
+        // GET: GroupsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var ips = iPaddresssRepository.GetIpByID(id);
+            IPAddressViewModel vm = new IPAddressViewModel();
+            vm.ID_IPAddress = ips.ID_IPAddress;
+            vm.IPAddressName = ips.IPAddressName;
+            vm.Available = ips.Status;
+            return View(vm);
+        }
+
+        // POST: GroupsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IPAddressViewModel model)
+        {
+            try
+            {
+                var ip = iPaddresssRepository.GetIpByID(id);
+                if (ip == null)
+                {
+                    return NotFound();
+                }
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+                if (!IsIPValid(model.IPAddressName))
+                {
+                    ModelState.AddModelError("IPAddressName", "Địa chỉ IP không hợp lệ");
+                    return View("Edit", model);
+                }
+                var checkIp = iPaddresssRepository.GetIpByName(model.IPAddressName.Trim());
+                if (checkIp != null && checkIp.ID_IPAddress != id)
+                {
+                    ModelState.AddModelError("IPAddressName", "Địa chỉ đã tồn tại trong hệ thống.");
+                    return View("Edit", model);
+                }
+                ip.IPAddressName = model.IPAddressName.Trim();
+                ip.Status = model.Available;
+                iPaddresssRepository.UpdateIp(ip);
+                iPaddresssRepository.Save();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+
+
+        // POST: GroupsController/Delete/5
+        [HttpDelete]
+        public JsonResult Delete(int id)
+        {
+            try
+            {
+                var ip = iPaddresssRepository.GetIpByID(id);
+
+                if (ip != null)
+                {
+                    iPaddresssRepository.DeleteIp(id);
+                    iPaddresssRepository.Save();
+                }
+
+                return Json(new
+                {
+                    status = true
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    status = true
+                });
+            }
+        }
+    }
+}
---
+++ AnhKhoa/UsersController.cs
@@ -0,0 +1,186 @@
+﻿using IPAddressManagement.Areas.Admin.Models;
+using IPAddressManagement.Areas.Admin.Repositories;
+using IPAddressManagement.Common;
+using IPAddressManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IPAddressManagement.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private IUserRepository userRepository;
+        private IGroupRepository groupRepository;
+
+        public UsersController(IUserRepository userRepository, IGroupRepository groupRepository)
+        {
+            this.userRepository = userRepository;
+            this.groupRepository = groupRepository;
+        }
+        // GET: UsersController
+        public ActionResult Index()
+        {
+            var users = userRepository.GetUsers();
+            return View(users);
+        }
+
+        // GET: UsersController/Details/5
+        public ActionResult Details(int id)
+        {
+            var users = userRepository.GetUserByID(id);
+            return View(users);
+        }
+
+        // GET: UsersController/Create
+        public ActionResult Create()
+        {
+            var groups = groupRepository.GetGroups();
+            ViewBag.Groups = groups;
+            return View(new UserViewModel());
+        }
+
+        // POST: UsersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UserViewModel model)
+        {
+            try
+            {
+                // Kiểm tra dữ liệu đầu vào hợp lệ
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+                // Kiểm tra email đã tồn tại
+                if (userRepository.ExistsUserByEmail(model.Email.Trim())) 
+                {
+                    ModelState.AddModelError("Email", "Tên email đã tồn tại trong hệ thống.");
+                    return View("Create", model);
+                }
+                // Tạo đối tượng GroupUser mới và gán giá trị từ model vào đối tượng này.
+                User u = new User();
+                u.FirstName = model.FirstName;
+                u.LastName = model.LastName;
+                u.Email = model.Email;
+                if (model.NewPassWord != null)
+                {
+                    string encryptedPass = Encryptor.MD5Hash(model.NewPassWord.Trim());
+                    u.PassWord = encryptedPass;
+                }
+                else
+                {
+                    string encryptedPass = Encryptor.MD5Hash("123456");
+                    u.PassWord = encryptedPass;
+                }
+                u.PhoneNumber = model.PhoneNumber;
+                u.ID_Group = model.ID_Group;
+
+                // Lưu đối tượng GroupUser mới vào cơ sở dữ liệu.
+                userRepository.InsertUser(u);
+                userRepository.Save();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                // Handle the exception here
+                return View(model);
+            }
+        }
+
+        // GET: UsersController/Edit/5
+        public ActionResult Edit(int id)
+        {          
+            var groups = groupRepository.GetGroups();
+            ViewBag.Groups = groups;
+
+            var user = userRepository.GetUserByID(id);
+            UserViewModel model = new UserViewModel();
+            model.ID_User = user.ID_User;
+            model.FirstName = user.FirstName;
+            model.LastName = user.LastName;
+            model.Email = user.Email;
+            model.PhoneNumber = user.PhoneNumber;
+            model.ID_Group = user.ID_Group.HasValue ? (int) user.ID_Group : 0;
+            return View(model);
+        }
+
+        // POST: UsersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, UserViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }            
+
+                var user = userRepository.GetUserByID(id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                var userCheck = userRepository.GetUserByEmail(model.Email.Trim());
+                if (userCheck != null && userCheck.ID_User != model.ID_User)
+                {
+                    ModelState.AddModelError("Email", "Tên email đã tồn tại trong hệ thống.");
+                    return View("Create", model);
+                }
+
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.Email = model.Email;
+                if (model.NewPassWord != null)
+                {
+                    string encryptedPass = Encryptor.MD5Hash(model.NewPassWord.Trim());
+                    user.PassWord = encryptedPass;
+                }
+                user.PhoneNumber = model.PhoneNumber;
+                user.ID_Group = model.ID_Group;
+
+                userRepository.UpdateUser(user);
+                userRepository.Save();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+
+        // Delete: UsersController/Delete/5
+        [HttpDelete]
+        public JsonResult Delete(int id)
+         {
+            try
+            {
+                var user = userRepository.GetUserByID(id);
+
+                if (user != null)
+                {
+                    userRepository.DeleteUser(id);
+                    userRepository.Save();
+                }
+
+                return Json(new
+                {
+                    status = true
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    status = true
+                });
+            }
+        }
+    }
+}
